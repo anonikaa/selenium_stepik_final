c8bff12c624b749875063a90e5508a4cf7c03316
Anastasia <anonikaa@gmail.com>
2020-04-10 13:14:17 +0300

    add Page Object product page

---
+++ pages/product_page.py
@@ -0,0 +1,31 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket.click()
+
+    def should_be_add_to_basket(self):
+        self.should_be_message()
+        self.should_be_right_price()
+
+    def should_be_message(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_MESSAGE).text
+        print(product_name, message)
+        assert message == product_name, f"{product_name} is not {message}"
+
+    def should_be_right_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+        print(product_price, basket_price)
+        assert product_price == basket_price, f"{product_price} not equal {basket_price}"
+
+
+
+
+
+
+
+
