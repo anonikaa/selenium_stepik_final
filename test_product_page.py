ad2b9cc20f5432817160423275a4c30e5ba7d427
Anastasia <anonikaa@gmail.com>
2020-04-10 18:38:47 +0300

    add xfail

--- test_product_page.py
+++ test_product_page.py
@@ -2,21 +2,31 @@ import pytest
 from .pages.product_page import ProductPage
 
 
-@pytest.mark.parametrize('link', ["?promo=offer0",
-                                  "?promo=offer1",
-                                  "?promo=offer2",
-                                  "?promo=offer3",
-                                  "?promo=offer4",
-                                  "?promo=offer5",
-                                  "?promo=offer6",
-                                  "?promo=offer7",
-                                  "?promo=offer8",
-                                  "?promo=offer9"])
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
 def test_guest_can_add_product_to_basket(browser, link):
-    current_link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{link}/"
-    page = ProductPage(browser, current_link)
+    page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_add_to_basket()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
