0b47df8b3a0c2da1a4133533f7a771789e210f16
Anastasia <anonikaa@gmail.com>
2020-04-10 13:11:47 +0300

    add alert accept

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,8 @@
+import math
+import time
+
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -16,3 +20,18 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self): #метод для проверки заданий на степике
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+            time.sleep(30)
+        except NoAlertPresentException:
+            print("No second alert presented")
