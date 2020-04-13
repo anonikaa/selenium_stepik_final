341e955ddec3301141eaf6e807978cddbcd142da
Anastasia <anonikaa@gmail.com>
2020-04-13 19:19:40 +0300

    add page object basket page

---
+++ pages/basket_page.py
@@ -0,0 +1,13 @@
+import time
+from .base_page import BasePage
+from .locators import BasePageLocators
+from  .locators import BasketPageLocators
+
+class BasketPage(BasePage):
+    def should_be_empty_basket(self):
+        time.sleep(3)
+        assert self.is_not_element_present(*BasketPageLocators.ITEMS_TO_BUY_NOW), "There are items in the basket"
+
+    def should_be_message_empty_basket(self):
+        time.sleep(3)
+        assert self.is_element_present(*BasketPageLocators.MESSAGE_EMPTY_BASKET), "No message that basket is empty"
