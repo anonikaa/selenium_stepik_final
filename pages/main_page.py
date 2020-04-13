643dd1740256c17b9ce129d375d2819376a8655f
Anastasia <anonikaa@gmail.com>
2020-04-13 19:21:16 +0300

    refactoring (i have no idea)

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,7 @@
 from .base_page import BasePage
-from .login_page import LoginPage
-from .locators import MainPageLocators
-from selenium.webdriver.common.by import By
+
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-        # return LoginPage(browser=self.browser, url=self.browser.current_url)
-
-    def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    #я ваще хз что этот метод сделал тут
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
