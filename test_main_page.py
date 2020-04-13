60fb6cf07aa645f546690b8b36594684007e7b62
Anastasia <anonikaa@gmail.com>
2020-04-13 19:11:46 +0300

    add open basket from main page test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,10 @@
+import time
+
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+
+# pytest -v -s --tb=line --language=en test_main_page.py
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
@@ -15,5 +20,12 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_message_empty_basket()
 
-# pytest -v --tb=line --language=en test_main_page.py
