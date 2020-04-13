da06c5e89526c76b9c3908187827ec7c37cedfa3
Anastasia <anonikaa@gmail.com>
2020-04-13 19:18:40 +0300

    add some function

--- pages/product_page.py
+++ pages/product_page.py
@@ -22,6 +22,9 @@ class ProductPage(BasePage):
         print(product_price, basket_price)
         assert product_price == basket_price, f"{product_price} not equal {basket_price}"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.ADD_TO_BASKET_MESSAGE), \
+            "Success message is presented, but should not be"
 
 
 
